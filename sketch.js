841763887f89623705c96b367d8851744d909f02
Sravya2007 <sravyamarepally@outlook.com>
2020-06-10 08:32:37 +0530

    Change in sketch.js

--- sketch.js
+++ sketch.js
@@ -56,7 +56,7 @@ function draw() {
 
 function keyPressed() {
 	if(keyCode === UP_ARROW) {
-		Matter.Body.applyForce(paper1.body, paper1.body.position, {x: 130, y:-145});
+		Matter.Body.applyForce(paper1.body, paper1.body.position, {x: 283, y:-302});
 	}
   
 }
